e6b5f9436a852c3c20364c5920202e6da038e4ae
abijaode~ <abijaode123@gmail.com>
2025-08-13 17:00:08 -0300

    mudanças no codigo

---
+++ aula2/teste.py
@@ -0,0 +1,10 @@
+a = print ("escreva o primeiro numero")
+#input (a)
+b = print ("escreva o segundo numero")
+#input (b)
+c = a - b
+''' if a==b:
+    print (f" {a}")
+else:
+    print (f"{c}")
+    if else nao vai funcionar, usar o while '''
