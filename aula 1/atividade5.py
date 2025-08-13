42ec7d68365b6d9eb3dd8a9fa6478d14e4c6491c
abijaode~ <abijaode123@gmail.com>
2025-08-13 16:05:46 -0300

    pasta nova

---
+++ aula 1/atividade5.py
@@ -0,0 +1,6 @@
+# O codigo vai identificar o nome da pessoa na variavel, depois vai dizer olá e inserir o nome registrado na variavel
+
+x = "qual o seu nome?"
+y = input(x)
+z = f"ola,{y} =)"
+print(z)
