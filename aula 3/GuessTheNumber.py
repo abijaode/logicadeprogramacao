3f24c6c17d9dcfd31ee49389ba0352020a3646c1
Lucas Abijaode <abijaode123@gmail.com>
2025-08-20 17:32:43 -0300

    colocando o numero aleatorio

--- aula 3/GuessTheNumber.py
+++ aula 3/GuessTheNumber.py
@@ -1,17 +1,22 @@
-numero = 5
-#import random
+import random
+numero = random.randint(1, 10) 
+
+nome = input("Digite seu nome: ")
+print("Olá,", nome, "! Vamos jogar um jogo de adivinhação.")
+
 
 tentativa = input("Advinhe o número entre 1 e 10: ")
 tentativa = int(tentativa)
+
+numerotentativa = 1
 
 while tentativa != numero:
     if tentativa < numero:
         print("Muito baixo! Tente novamente.")
     elif tentativa > numero:
         print("Muito alto! Tente novamente.")
-
+    numerotentativa = numerotentativa + 1
     tentativa = input("Advinhe o número entre 1 e 10: ")
     tentativa = int(tentativa)
 
-print("Parabéns! Você adivinhou o número:", numero)
-
+print("Parabéns", nome, "! Você adivinhou o número:", numero, "em", numerotentativa, "tentativas.")
