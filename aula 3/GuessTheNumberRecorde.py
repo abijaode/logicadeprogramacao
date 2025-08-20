387842be8f5d6561bd42f1223d874300a1645a70
Lucas Abijaode <abijaode123@gmail.com>
2025-08-20 18:30:15 -0300

    exercicio novo (nao está funcionando)

---
+++ aula 3/GuessTheNumberRecorde.py
@@ -0,0 +1,42 @@
+import random
+
+novamente = "s"
+while novamente == "s":
+
+
+
+    numero = random.randint(1, 10) 
+
+    nome = input("Digite seu nome: ")
+    print("Olá,", nome, "! Vamos jogar um jogo de adivinhação.")
+
+
+    tentativa = input("Advinhe o número inteiro entre 1 e 10: ")
+    tentativa = int(tentativa)
+
+    numerotentativa = 1
+
+    while tentativa != numero:
+        if tentativa < numero:
+            print("Muito baixo! Tente novamente.")
+        elif tentativa > numero:
+            print("Muito alto! Tente novamente.")
+        numerotentativa = numerotentativa + 1
+        tentativa = input("Advinhe o número inteiro entre 1 e 10: ")
+        tentativa = int(tentativa)
+
+    print("Parabéns", nome, "! Você adivinhou o número:", numero, "em", numerotentativa, "tentativas.")
+    novamente = input("Gostaria de jogar novamente? (s/n)")
+
+recorde = 0
+
+if recorde == numerotentativa:
+
+    elif recorde == 0 or numerotentativa > recorde:
+
+    else recorde = numerotentativa:
+
+
+print("Seu recorde é de", recorde, "tentativas.")
+
+print("Obrigado por jogar!")
