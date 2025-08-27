53387705a6d85455699fc8d02d51483855d7b6c3
Lucas Abijaode <abijaode123@gmail.com>
2025-08-27 15:25:02 -0300

    Prova Horario, e criação do arquivo da aula

---
+++ Provas_e_atividades/ProvaHorario.py
@@ -0,0 +1,16 @@
+#hora = 15
+#extra = 22,5
+tempo = input("Digite o tempo em horas trabalhadas na semana: ")
+tempo = int(tempo)
+
+if tempo > 44:
+    extra = tempo - 44 
+    salario = 44 * 15 + extra * 22.5
+
+else:
+    extra = 0
+    salario = tempo * 15
+print("O seu salário semanal é de", salario, "reais. Sendo ", extra, "horas extras e ", tempo - extra, "horas normais.")
+
+
+    
